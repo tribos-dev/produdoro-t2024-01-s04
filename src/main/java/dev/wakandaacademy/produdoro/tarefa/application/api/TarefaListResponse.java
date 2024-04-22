a877fa61d3204763acc80c8eb3d9706d516d8002
christianlacerdahotmail <christianlacerda@hotmail.com>
2024-04-22 19:47:49 -0300

    implementa camada API

---
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaListResponse.java
@@ -0,0 +1,32 @@
+package dev.wakandaacademy.produdoro.tarefa.application.api;
+
+import java.util.UUID;
+
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusAtivacaoTarefa;
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusTarefa;
+import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+import lombok.Getter;
+
+@Getter
+public class TarefaListResponse {
+	private UUID idTarefa;
+    private String descricao;
+    private UUID idUsuario;
+    private UUID idArea;
+    private UUID idProjeto;
+    private StatusTarefa status;
+    private StatusAtivacaoTarefa statusAtivacao;
+    private int contagemPomodoro;
+
+    public TarefaListResponse(Tarefa tarefa) {
+        this.idTarefa = tarefa.getIdTarefa();
+        this.descricao = tarefa.getDescricao();
+        this.idUsuario = tarefa.getIdUsuario();
+        this.idArea = tarefa.getIdArea();
+        this.idProjeto = tarefa.getIdProjeto();
+        this.status = tarefa.getStatus();
+        this.statusAtivacao = tarefa.getStatusAtivacao();
+        this.contagemPomodoro = tarefa.getContagemPomodoro();
+    }
+
+}
