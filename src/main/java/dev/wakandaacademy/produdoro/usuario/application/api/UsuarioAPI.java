208ce21e973f1a78b8f56def41478ad41cbd5299
wanderson648 <testepedroca@gmail.com>
2024-04-21 22:42:15 -0300

    implementa camada de API

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
@@ -17,4 +17,8 @@ public interface UsuarioAPI {
 	@GetMapping(value = "/{idUsuario}")
 	@ResponseStatus(code = HttpStatus.OK)
 	UsuarioCriadoResponse buscaUsuarioPorId(@PathVariable UUID idUsuario);
+
+    @PatchMapping("/{idUsuario}/foco")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    void mudaStatusParaFoco(@RequestHeader("Authorization") String token, @PathVariable UUID idUsuario);
 }
