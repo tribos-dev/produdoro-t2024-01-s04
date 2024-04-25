59db7ff6e8b1a2e06dc036aebc81692c3cfa4162
Joanerson Costa <joanersonc31@gmail.com>
2024-04-24 22:11:46 -0300

    implementa teste deleta todas as tarefas do usuario com sucesso

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -1,25 +1,30 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.wakandaacademy.produdoro.DataHelper;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 
 @ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
@@ -31,6 +36,8 @@ class TarefaApplicationServiceTest {
     //	@MockBean
     @Mock
     TarefaRepository tarefaRepository;
+    @Mock
+    UsuarioRepository usuarioRepository;
 
     @Test
     void deveRetornarIdTarefaNovaCriada() {
@@ -44,10 +51,24 @@ class TarefaApplicationServiceTest {
         assertEquals(UUID.class, response.getIdTarefa().getClass());
     }
 
-
-
     public TarefaRequest getTarefaRequest() {
         TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
         return request;
+    }
+    
+    @Test
+    @DisplayName("Deleta todas as tarefas do usuario com sucesso")
+    void deletaTodasAsTarefasDoUsuario_comDadosValidos_sucesso() {
+    	Usuario usuario = DataHelper.createUsuario();
+    	List<Tarefa> tarefasDoUsuario = DataHelper.createListTarefa();
+    	String email = usuario.getEmail();
+    	UUID idUsuario = usuario.getIdUsuario();
+    	when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+    	when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+    	when(tarefaRepository.buscaTodasAsTarefasDoUsuario(any())).thenReturn(tarefasDoUsuario);
+
+    	tarefaApplicationService.deletaTodasAsTarefasDoUsuario(email, idUsuario);
+    	
+    	verify(tarefaRepository, times(1)).deletaTodasAsTarefasDoUsuario(tarefasDoUsuario);
     }
 }
